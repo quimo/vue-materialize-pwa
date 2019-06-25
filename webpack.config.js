420c4086d8eeb4818be10eb084af7c63c5f0c424
Simone Alati <git@simonealati.it>
2019-06-25 10:48:34 +0200

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const SWPrecache = require('sw-precache-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: "production",
     plugins: [
+        //plugin per usare i service worker
         new SWPrecache({
             cacheId: 'demoapp',
             filepath: 'service-worker.js',
@@ -12,6 +14,14 @@ module.exports = {
             'manifest.json',
             ],
             stripPrefix: '/'
-        })
+        }),
+        //plugin per copiare file statici tra src e dist
+        new CopyPlugin([
+            { from: './node_modules/materialize-css/dist/css/materialize.min.css', to: './materialize.min.css' },
+            { from: './node_modules/materialize-css/dist/js/materialize.min.js', to: './materialize.min.js' },
+            { from: './src/manifest.json', to: './manifest.json' },
+            { from: './src/style.css', to: './style.css' },
+            { from: './src/images/', to: './images/' }
+        ])
     ]
 }
